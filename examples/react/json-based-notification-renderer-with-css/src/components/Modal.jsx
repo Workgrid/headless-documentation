fdaebeae9949bc3575a590744ee3ca117f0ce696
Eric Routhier <Eric.Routhier@workgrid.com>
2022-12-01 15:50:38 -0500

    ETSWORK-16638 clean up modal logic

--- examples/react/json-based-notification-renderer-with-css/src/components/Modal.jsx
+++ examples/react/json-based-notification-renderer-with-css/src/components/Modal.jsx
@@ -17,9 +17,9 @@ const modalStyle = {
 Modal.setAppElement('#root')
 
 export default function BasicModal({ open, handleClose, payload }) {
-  if (payload) {
-    return (
-      <div>
+  return (
+    <div>
+      {payload && (
         <Modal isOpen={open} onRequestClose={handleClose} style={modalStyle} contentLabel="Example Modal">
           <AdaptiveCardUsingHostConfigContext payload={payload} />
           <div style={{ display: 'flex', justifyContent: 'right' }}>
@@ -28,8 +28,7 @@ export default function BasicModal({ open, handleClose, payload }) {
             </button>
           </div>
         </Modal>
-      </div>
-    )
-  }
-  return <div></div>
+      )}
+    </div>
+  )
 }
