c7bcbef3966216685095233ad3021f23e73c3c34
Eric Routhier <Eric.Routhier@workgrid.com>
2022-12-02 11:48:38 -0500

    ETSWORK-16638 add slide up to modal

--- examples/react/json-based-notification-renderer-with-custom-components/src/components/Modal.jsx
+++ examples/react/json-based-notification-renderer-with-custom-components/src/components/Modal.jsx
@@ -2,12 +2,14 @@ import * as React from 'react'
 import Box from '@mui/material/Box'
 import Modal from '@mui/material/Modal'
 import Button from '@mui/material/Button'
+import Slide from '@mui/material/Slide'
+import Backdrop from '@mui/material/Backdrop'
 import { AdaptiveCardUsingHostConfigContext } from 'adaptivecards-react'
 
-const style = {
+const modalStyle = { display: 'flex', alignItems: 'center', justifyContent: 'center' }
+
+const boxStyle = {
   position: 'absolute',
-  top: '50%',
-  left: '50%',
   transform: 'translate(-50%, -50%)',
   width: '700px',
   bgcolor: 'background.paper',
@@ -17,14 +19,20 @@ const style = {
 
 const BasicModal = ({ open, handleClose, payload }) => {
   return (
-    <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
+    <Modal
+      style={modalStyle}
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Slide in={open} direction="up">
+        <Box sx={boxStyle}>
           <AdaptiveCardUsingHostConfigContext payload={payload} />
           <div style={{ display: 'flex', justifyContent: 'right' }}>
             <Button variant="outlined" onClick={handleClose}>
@@ -32,8 +40,8 @@ const BasicModal = ({ open, handleClose, payload }) => {
             </Button>
           </div>
         </Box>
-      </Modal>
-    </div>
+      </Slide>
+    </Modal>
   )
 }
 export default BasicModal
